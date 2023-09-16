f1f6f66c9bc45f4e53c2a18346be22cd6970df2a
Aidan Bennett <57372629+aidanjbennett@users.noreply.github.com>
2023-09-16 16:21:34 +0100

    :mag: Add sitemap
    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: 'https://www.aidanjbennett.com',
   experimental: {
     assets: true,
